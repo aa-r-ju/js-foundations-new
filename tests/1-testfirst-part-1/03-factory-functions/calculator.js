2bc549ee96cd0473f8c660f10552823a8dc53d64
aa-r-ju <aarju3447@gmail.com>
2026-02-04 20:10:29 +0545

    feat: calculator.js

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,1 +1,68 @@
 /* eslint-disable no-unused-vars */
+function createCalculator() {
+  let total = 0;
+
+  return {
+    add(num) {
+      total += num;
+    },
+    subtract(num) {
+      total -= num;
+    },
+    value() {
+      return total;
+    },
+    clear() {
+      total = 0;
+    },
+  };
+}
+
+function addSquareMethod(instances) {
+  for (let i = 0; i < instances.length; i++) {
+    instances[i].square = function () {
+      const value = this.value();
+      return value * value;
+    };
+  }
+
+  return instances;
+}
+// const humanCalculatorMethods = {
+//   add(num) {
+//     this.total += num;
+//   },
+//   subtract(num) {
+//     this.total -= num;
+//   },
+//   value() {
+//     return this.total;
+//   },
+//   clear() {
+//     this.total = -10;
+//   },
+// };
+
+const humanCalculatorMethods = {
+  add(num) {
+    this.total += num;
+  },
+
+  subtract(num) {
+    this.total -= num;
+  },
+
+  value() {
+    return this.total;
+  },
+
+  clear() {
+    this.total = -10;
+  },
+};
+
+function createHumanCalculator() {
+  const calculator = Object.create(humanCalculatorMethods);
+  calculator.total = -10;
+  return calculator;
+}
