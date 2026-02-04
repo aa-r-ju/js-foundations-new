bf81746a5407df4697c905e6f0df2d722581f912
aa-r-ju <aarju3447@gmail.com>
2026-02-04 19:38:29 +0545

    feat: object-syntax

--- tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
+++ tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+let loud = "loud";
+let name = "Cody";
+let dog = {
+  bark() {
+    return "ruff ruff!";
+  },
+  name,
+  loudBark() {
+    return "RUFF RUFF!";
+  },
+};
+
+console.log(dog);
