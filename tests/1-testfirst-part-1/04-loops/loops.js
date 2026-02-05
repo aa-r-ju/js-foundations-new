5123de23acc384f208a282fc073462f74dd4356b
aa-r-ju <aarju3447@gmail.com>
2026-02-05 18:30:06 +0545

    feat: repeat of loops

--- tests/1-testfirst-part-1/04-loops/loops.js
+++ tests/1-testfirst-part-1/04-loops/loops.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function repeat(str, value) {
+  let repeatedValue = "";
+  if (value > 0) {
+    for (let i = 0; i < value; i++) {
+      repeatedValue += str;
+    }
+  }
+  return repeatedValue;
+}
+
+console.log(repeat("yo", 2));
