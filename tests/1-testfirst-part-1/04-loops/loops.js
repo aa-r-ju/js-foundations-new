4106bc02a1f8afdb2f68d4d8203526bbfb8a3f70
aa-r-ju <aarju3447@gmail.com>
2026-02-05 18:37:04 +0545

    feat: sum of loops

--- tests/1-testfirst-part-1/04-loops/loops.js
+++ tests/1-testfirst-part-1/04-loops/loops.js
@@ -10,3 +10,15 @@ function repeat(str, value) {
 }
 
 console.log(repeat("yo", 2));
+
+function sum(array) {
+  let numberValue = 0;
+  for (let i = 0; i < array.length; i++) {
+    numberValue += array[i];
+  }
+  return numberValue;
+}
+console.log(sum([]));
+console.log(sum([7]));
+console.log(sum([7, 11]));
+console.log(sum([1, 3, 5, 7, 9]));
