5102d1c36f47ee92cff20b1d1bf15bdde5964a6c
aa-r-ju <aarju3447@gmail.com>
2026-02-06 16:33:53 +0545

    feat: findObjPropsHasOwn

--- tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
+++ tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function findObjPropsHasOwn(obj) {
+  let array = [];
+  for (let keys in obj) {
+    if (obj.hasOwnProperty(keys)) {
+      array.push(keys);
+    }
+  }
+  return array.join(", ");
+}
