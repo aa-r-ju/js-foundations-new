8a56ff83390e895eb2a3c5f76a1c396897830ca2
aa-r-ju <aarju3447@gmail.com>
2026-02-07 19:01:14 +0545

    feat: createBook

--- tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
+++ tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
@@ -1,1 +1,36 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+// Prototype object that will hold shared methods
+let bookMethods = {
+  getPrice: function () {
+    return this.price;
+  },
+  getId: function () {
+    return this.id;
+  },
+  getInfo: function () {
+    return `${this.title} by ${this.author}`;
+  },
+  addRating: function (rating) {
+    this.rating.push(rating);
+  },
+  getRating: function () {
+    if (this.rating.length === 0) return 0;
+    let totalStars = 0;
+    for (let i = 0; i < this.rating.length; i++) {
+      totalStars += this.rating[i].length;
+    }
+    return Math.round(totalStars / this.rating.length);
+  },
+};
+
+function createBook(id, title, author, price) {
+  let book = Object.create(bookMethods);
+
+  book.id = id;
+  book.title = title;
+  book.author = author;
+  book.price = price;
+  book.rating = [];
+
+  return book;
+}
