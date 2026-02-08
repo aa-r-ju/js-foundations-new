0da92998d09d9b726eb34aa345c258c4f0fcf92d
aa-r-ju <aarju3447@gmail.com>
2026-02-08 13:14:18 +0545

    feat: soup

--- tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
+++ tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function soup(phrase, characterBank) {
+  let bank = characterBank.split("");
+
+  for (let i = 0; i < phrase.length; i++) {
+    let char = phrase[i];
+    let index = bank.indexOf(char);
+
+    if (index === -1) {
+      return false;
+    }
+    bank.splice(index, 1);
+  }
+  return true;
+}
+console.log(soup("helloworld", "dlhelolwor"));
