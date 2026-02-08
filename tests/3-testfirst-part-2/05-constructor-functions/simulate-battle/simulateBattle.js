c04703e56814d84a493cc212ae308714debddf10
aa-r-ju <aarju3447@gmail.com>
2026-02-08 21:05:31 +0545

    feat: Pokemon

--- tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
+++ tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
+function Pokemon(name, health, attackBonus) {
+  (this.name = name), (this.health = health), (this.attackBonus = attackBonus);
+}
+Pokemon.prototype.biteAttack = function () {
+  return this.attackBonus + 2;
+};
+Pokemon.prototype.isDefeated = function () {
+  return this.health <= 0;
+};
+
+let human = new Pokemon("pikachu", 250, 4);
+console.log(human.biteAttack());
+console.log(human.isDefeated());
