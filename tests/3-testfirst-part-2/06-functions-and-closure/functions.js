e45e9db6c1c44a5aecf5ddded192929fffeb34d1
aa-r-ju <aarju3447@gmail.com>
2026-02-09 19:51:48 +0545

    feat: yourFunctionRunner

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -6,3 +6,22 @@ function concatString() {
 
 console.log(concatString("David", " ", "Yang"));
 console.log(concatString("This", " ", "should", " ", "be done with join"));
+
+function yourFunctionRunner() {
+  let values = Array.from(arguments);
+  let result = "";
+  for (let i = 0; i < values.length; i++) {
+    result += values[i]();
+  }
+  return result;
+}
+
+const callThisFunction = () => {
+  return "Called Value";
+};
+
+const andThisFunction = () => {
+  return " and Other Value";
+};
+
+console.log(yourFunctionRunner(callThisFunction, andThisFunction));
