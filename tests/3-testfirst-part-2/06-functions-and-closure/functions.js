876f11b96588ae7d09fbb1bc2e4864a752ef21e3
aa-r-ju <aarju3447@gmail.com>
2026-02-09 18:26:38 +0545

    feat: concatString

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function concatString() {
+  const values = Array.from(arguments);
+  return values.join("");
+}
+
+console.log(concatString("David", " ", "Yang"));
+console.log(concatString("This", " ", "should", " ", "be done with join"));
