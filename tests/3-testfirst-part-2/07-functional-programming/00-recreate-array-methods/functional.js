79761cb24c6590d355ae6bdff8287090a52d0d4d
aa-r-ju <aarju3447@gmail.com>
2026-02-13 11:27:22 +0545

    feat: filter

--- tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
+++ tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
@@ -23,4 +23,25 @@ let arr = [1, 2, 3];
 function doubler(num) {
   return num * 2;
 }
-console.log(map(arr, doubler));
+
+function filter(array, callback) {
+  let newFilter = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      newFilter.push(array[i]);
+    }
+  }
+  return newFilter;
+}
+
+const evenFilter = (element) => {
+  if (element % 2 === 0) return true;
+  else return false;
+};
+
+const oddFilter = (element) => {
+  return !evenFilter(element);
+};
+let values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(filter(values, evenFilter));
+console.log(filter(values, oddFilter));
