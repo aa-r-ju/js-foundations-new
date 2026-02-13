04128f020f6d28160c2468679eee28f8c8dc0e84
aa-r-ju <aarju3447@gmail.com>
2026-02-13 11:02:26 +0545

    feat: map

--- tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
+++ tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
@@ -9,3 +9,18 @@ const array = ["soccer", "swimming", "softball", "water-polo"];
 forEach(array, function (val) {
   console.log(val);
 });
+
+function map(array, callback) {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i], i, array));
+  }
+
+  return newArray;
+}
+let arr = [1, 2, 3];
+function doubler(num) {
+  return num * 2;
+}
+console.log(map(arr, doubler));
