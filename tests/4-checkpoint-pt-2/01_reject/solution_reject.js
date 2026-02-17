3ede2f2ed75391d8859d3a98c6782173e6d61d1f
aa-r-ju <aarju3447@gmail.com>
2026-02-17 19:59:51 +0545

    feat: reject

--- tests/4-checkpoint-pt-2/01_reject/solution_reject.js
+++ tests/4-checkpoint-pt-2/01_reject/solution_reject.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+function reject(array, callback) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!callback(array[i])) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
