4e97b0a8171c7a909bac76ee4f0b44a822f891b9
aa-r-ju <aarju3447@gmail.com>
2026-02-17 20:11:28 +0545

    feat: useMapToUpperCase

--- tests/4-checkpoint-pt-2/02_array-methods/solutions_array_methods.js
+++ tests/4-checkpoint-pt-2/02_array-methods/solutions_array_methods.js
@@ -1,1 +1,6 @@
 /* eslint-disable no-unused-vars */
+function useMapToUpperCase(str) {
+  let values = str.split(" ");
+  return values.map((word) => word.toUpperCase());
+}
+console.log(useMapToUpperCase("Keep It Simple"));
