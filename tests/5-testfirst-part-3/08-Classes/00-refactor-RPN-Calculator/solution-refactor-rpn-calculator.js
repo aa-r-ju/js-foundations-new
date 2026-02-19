faa48277f6f6eebaae89a808d5e13bee54f63e84
aa-r-ju <aarju3447@gmail.com>
2026-02-19 22:09:11 +0545

    feat: RPNCalculator

--- tests/5-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
+++ tests/5-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
@@ -3,3 +3,52 @@
 /*
   Refactor the constructor function below and use ES2015 Class Syntax
 */
+/* eslint-disable no-unused-vars */
+
+class RPNCalculator {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(num) {
+    this.stack.push(num);
+  }
+
+  value() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  _checkLength() {
+    if (this.stack.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    }
+  }
+
+  plus() {
+    this._checkLength();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a + b);
+  }
+
+  minus() {
+    this._checkLength();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a - b);
+  }
+
+  times() {
+    this._checkLength();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a * b);
+  }
+
+  divide() {
+    this._checkLength();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a / b);
+  }
+}
