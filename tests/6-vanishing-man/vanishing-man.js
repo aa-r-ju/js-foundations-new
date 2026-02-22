0d3562ad7a598d729ce0046c812ab6cb82b0702a
aa-r-ju <aarju3447@gmail.com>
2026-02-22 20:21:46 +0545

    feat: VanishingMan class declare

--- tests/6-vanishing-man/vanishing-man.js
+++ tests/6-vanishing-man/vanishing-man.js
@@ -53,3 +53,26 @@ const ASCIIART = [
 
 =========`,
 ];
+
+class VanishingMan {
+  constructor(secretWord, remainingGuesses, lettersGuessed, gameState) {
+    this.secretWord = secretWord.toLowerCase().split("");
+    this.remainingGuesses = 6;
+    this.lettersGuessed = [];
+    this.gameState = "playing";
+  }
+}
+
+const kk = new VanishingMan("aerobic");
+console.log(kk.submitGuess("a"));
+console.log(kk.submitGuess("e"));
+console.log(kk.submitGuess("r"));
+console.log(kk.submitGuess("l"));
+console.log(kk.submitGuess("k"));
+console.log(kk.submitGuess("o"));
+console.log(kk.submitGuess("b"));
+console.log(kk.submitGuess("i"));
+console.log(kk.submitGuess("c"));
+console.log(kk.gameState);
+console.log(kk.computeGameState());
+console.log(kk.gameState);
