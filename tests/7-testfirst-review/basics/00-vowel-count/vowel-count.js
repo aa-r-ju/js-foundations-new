994a46825e420025c1b0d05366c3287695594a60
aa-r-ju <aarju3447@gmail.com>
2026-02-25 22:37:44 +0545

    feat: vowelsCount

--- tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
+++ tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+function vowelsCount(str) {
+  const result = {
+    a: 0,
+    e: 0,
+    i: 0,
+    o: 0,
+    u: 0,
+    total: 0,
+  };
+
+  return str
+    .toLowerCase()
+    .split("")
+    .reduce((acc, char) => {
+      if (acc.hasOwnProperty(char)) {
+        acc[char]++;
+        acc.total++;
+      }
+      return acc;
+    }, result);
+}
