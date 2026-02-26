db3aa23d08c6a34bb9f99f31e99c488e04be05f9
aa-r-ju <aarju3447@gmail.com>
2026-02-26 22:27:53 +0545

    feat: pigify

--- tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
+++ tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
@@ -1,1 +1,32 @@
 /* eslint-disable no-unused-vars */
+function pigify(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  return str
+    .split(" ")
+    .map((word) => {
+      if (vowels.includes(word[0])) {
+        return word + "ay";
+      }
+
+      if (word.startsWith("sch")) {
+        return word.slice(3) + "schay";
+      }
+
+      if (word[1] === "q" && word[2] === "u") {
+        return word.slice(3) + word.slice(0, 3) + "ay";
+      }
+
+      if (word.startsWith("qu")) {
+        return word.slice(2) + "quay";
+      }
+
+      let i = 0;
+      while (!vowels.includes(word[i])) {
+        i++;
+      }
+
+      return word.slice(i) + word.slice(0, i) + "ay";
+    })
+    .join(" ");
+}
