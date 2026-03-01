b47a1dae8246ddb96843888cad19a7f2ad5bc021
aa-r-ju <aarju3447@gmail.com>
2026-03-01 16:11:34 +0545

    feat: renameFiles

--- tests/7-testfirst-review/basics/03-rename-files/rename-files.js
+++ tests/7-testfirst-review/basics/03-rename-files/rename-files.js
@@ -1,1 +1,64 @@
 /* eslint-disable no-unused-vars */
+function renameFiles(files) {
+  const usedNames = new Set();
+  const nameCount = {};
+  const result = [];
+
+  for (let file of files) {
+    // If file name is not used yet
+    if (!usedNames.has(file)) {
+      result.push(file);
+      usedNames.add(file);
+      nameCount[file] = 1;
+    } else {
+      // Find the smallest available (n)
+      let count = nameCount[file] || 1;
+      let newName = `${file}(${count})`;
+
+      while (usedNames.has(newName)) {
+        count++;
+        newName = `${file}(${count})`;
+      }
+
+      result.push(newName);
+      usedNames.add(newName);
+      nameCount[file] = count + 1;
+    }
+  }
+
+  return result;
+}
+
+console.log(renameFiles(["a", "b", "c", "a"]));
+
+//
+
+function renameFiles(files) {
+  const usedNames = {};
+  const nameCount = {};
+  const result = [];
+
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+
+    if (!usedNames[file]) {
+      result.push(file);
+      usedNames[file] = true;
+      nameCount[file] = 1;
+    } else {
+      let count = nameCount[file] || 1;
+      let newName = file + "(" + count + ")";
+
+      while (usedNames[newName]) {
+        count++;
+        newName = file + "(" + count + ")";
+      }
+
+      result.push(newName);
+      usedNames[newName] = true;
+      nameCount[file] = count + 1;
+    }
+  }
+
+  return result;
+}
