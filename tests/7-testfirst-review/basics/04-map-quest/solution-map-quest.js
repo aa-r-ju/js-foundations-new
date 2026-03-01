2cc7e95146587e56bb7433c5909623f6b3c34beb
aa-r-ju <aarju3447@gmail.com>
2026-03-01 21:17:56 +0545

    feat: mapQuest

--- tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
+++ tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-unused-vars */
+function mapQuest(directions) {
+  let obj = {
+    N: "S",
+    S: "N",
+    E: "W",
+    W: "E",
+  };
+
+  return directions.reduce((acc, char) => {
+    let last = acc[acc.length - 1];
+    if (obj[char] === last) {
+      acc.pop();
+    } else {
+      acc.push(char);
+    }
+    return acc;
+  }, []);
+}
+console.log(mapQuest(["N", "E", "S", "W"]));
+console.log(mapQuest(["S", "E", "W", "W"]));
