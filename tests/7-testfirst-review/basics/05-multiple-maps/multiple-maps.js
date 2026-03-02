255371222bf9310bb05d16abab07e2c1d73b13fd
aa-r-ju <aarju3447@gmail.com>
2026-03-02 22:49:37 +0545

    feat: multipleMaps

--- tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
+++ tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+function multipleMaps(values, maps) {
+  return values.map((value) => {
+    for (let obj of maps) {
+      if (value in obj) {
+        return obj[value];
+      }
+    }
+  });
+}
