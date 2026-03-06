1836b48b538b50ee608669b4aceebbe17cede6d4
aa-r-ju <aarju3447@gmail.com>
2026-03-06 13:35:15 +0545

    feat: Array.prototype.fiilter

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -9,8 +9,21 @@ Array.prototype.maap = function (callback) {
   return newArr;
 };
 
-console.log([1, 2, 3].maap((val) => val + 1));
-// [2,3,4]
+// console.log([1, 2, 3].maap((val) => val + 1));
+// // [2,3,4]
 
-console.log(["bear", "owl"].maap((val) => val.toUpperCase()));
-// ["BEAR","OWL"]
+// console.log(["bear", "owl"].maap((val) => val.toUpperCase()));
+// // ["BEAR","OWL"]
+
+Array.prototype.fiilter = function (callback) {
+  let newArr = [];
+  this.forEach((element) => {
+    if (callback(element)) {
+      newArr.push(element);
+    }
+  });
+  return newArr;
+};
+
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].fiilter((val) => val % 2 === 0));
+console.log(["bear", "owl", "mouse", "dog"].filter((val) => val.includes("o")));
