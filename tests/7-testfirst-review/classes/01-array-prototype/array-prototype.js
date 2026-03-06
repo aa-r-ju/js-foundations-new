e1db14875884c25cfbfe9f184149e655166cf7a1
aa-r-ju <aarju3447@gmail.com>
2026-03-06 13:45:47 +0545

    feat:Array.prototype.reeduce

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -25,5 +25,16 @@ Array.prototype.fiilter = function (callback) {
   return newArr;
 };
 
-console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].fiilter((val) => val % 2 === 0));
-console.log(["bear", "owl", "mouse", "dog"].filter((val) => val.includes("o")));
+// console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].fiilter((val) => val % 2 === 0));
+// console.log(["bear", "owl", "mouse", "dog"].filter((val) => val.includes("o")));
+
+Array.prototype.reeduce = function (callback, initialValue) {
+  let accumulator = initialValue;
+
+  this.forEach((element, index) => {
+    accumulator = callback(accumulator, element, index, this);
+  });
+
+  return accumulator;
+};
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].reeduce((a, b) => a + b, 0));
