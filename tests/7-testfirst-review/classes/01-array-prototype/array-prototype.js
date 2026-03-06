250f5b00f7e97c76856633baf01b7ff996e3a88b
aa-r-ju <aarju3447@gmail.com>
2026-03-06 13:12:03 +0545

    feat: Array.prototype.maap

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -1,1 +1,16 @@
-/* eslint-disable no-unused-vars */
+// /* eslint-disable no-unused-vars */
+Array.prototype.maap = function (callback) {
+  let newArr = [];
+
+  this.forEach(function (element) {
+    newArr.push(callback(element));
+  });
+
+  return newArr;
+};
+
+console.log([1, 2, 3].maap((val) => val + 1));
+// [2,3,4]
+
+console.log(["bear", "owl"].maap((val) => val.toUpperCase()));
+// ["BEAR","OWL"]
