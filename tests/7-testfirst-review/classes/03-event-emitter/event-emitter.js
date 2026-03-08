a5528ff0846aa02475d7aeb8d7195097d90f6103
aa-r-ju <aarju3447@gmail.com>
2026-03-08 14:17:50 +0545

    feat: MyEventEmitter

--- tests/7-testfirst-review/classes/03-event-emitter/event-emitter.js
+++ tests/7-testfirst-review/classes/03-event-emitter/event-emitter.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  addListener(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+
+    this.events[event].push(callback);
+  }
+
+  emit(event, ...args) {
+    if (!this.events[event]) return;
+
+    this.events[event].forEach((callback) => {
+      callback(...args);
+    });
+  }
+}
