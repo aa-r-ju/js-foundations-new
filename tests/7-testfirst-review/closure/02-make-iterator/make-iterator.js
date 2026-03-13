5ce48046de30e3ae38d350f777f25fa6b4eafb1e
aa-r-ju <aarju3447@gmail.com>
2026-03-13 16:43:12 +0545

    feat: makeIterator

--- tests/7-testfirst-review/closure/02-make-iterator/make-iterator.js
+++ tests/7-testfirst-review/closure/02-make-iterator/make-iterator.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-unused-vars */
+function makeIterator(arr = []) {
+  let index = 0;
+
+  return {
+    getNext() {
+      if (index < arr.length) {
+        const value = arr[index];
+        index++;
+        return { value: value, done: false };
+      } else {
+        index++;
+        return { value: undefined, done: true };
+      }
+    },
+
+    getIndex() {
+      return index;
+    },
+  };
+}
