b22f159fcaa3aaea42aa4936f87ac1b9cbb64202
aa-r-ju <aarju3447@gmail.com>
2026-03-17 22:06:58 +0545

    feat: rotater

--- tests/7-testfirst-review/closure/03-rotater/rotater.js
+++ tests/7-testfirst-review/closure/03-rotater/rotater.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-unused-vars */
+function rotater(str) {
+  let direction = "left";
+
+  return function (num) {
+    let len = str.length;
+
+    if (num === len) {
+      direction = direction === "left" ? "right" : "left";
+      return str;
+    }
+
+    num = num % len;
+
+    if (direction === "left") {
+      return str.slice(num) + str.slice(0, num);
+    } else {
+      return str.slice(len - num) + str.slice(0, len - num);
+    }
+  };
+}
