607141018ee9a76b66e5b34d0fc545552e2a39b4
aa-r-ju <aarju3447@gmail.com>
2026-03-22 22:30:21 +0545

    feat: callAll

--- tests/7-testfirst-review/this-keyword-context/00-call-all/call-all.js
+++ tests/7-testfirst-review/this-keyword-context/00-call-all/call-all.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+function callAll(obj, arr) {
+  let resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let result = arr[i].call(obj);
+    resultArr.push(result);
+  }
+  return resultArr;
+}
+
+const fnArr = [
+  function firstName() {
+    return this.first;
+  },
+
+  function lastName() {
+    return this.last;
+  },
+];
+const obj = { first: "Nimit", last: "Maru" };
+
+console.log(callAll(obj, fnArr));
