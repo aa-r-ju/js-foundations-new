5581fbfb597701131905983f5485a17e473034ba
aa-r-ju <aarju3447@gmail.com>
2026-03-23 22:48:22 +0545

    feat: doNotInclude

--- tests/7-testfirst-review/this-keyword-context/01-do-not-include/do-not-include.js
+++ tests/7-testfirst-review/this-keyword-context/01-do-not-include/do-not-include.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-extend-native */
+Array.prototype.doNotInclude = function (indexes) {
+  if (!Array.isArray(indexes)) {
+    indexes = [indexes];
+  }
+
+  return this.filter((_, i) => {
+    return !indexes.includes(i);
+  });
+};
