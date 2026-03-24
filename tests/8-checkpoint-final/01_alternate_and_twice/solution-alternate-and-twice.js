41a31cf949a8e7947dd7ec0460aa601b16b68d7a
aa-r-ju <aarju3447@gmail.com>
2026-03-24 17:41:33 +0545

    feat: twice

--- tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
+++ tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
@@ -8,3 +8,25 @@ function alternate(fn) {
     }
   };
 }
+
+function twice(fn) {
+  let count = 0;
+
+  return function () {
+    if (count < 2) {
+      count++;
+
+      return fn();
+    } else {
+      return 0;
+    }
+  };
+}
+
+const returnTen = twice(() => {
+  return 10;
+});
+console.log(returnTen());
+console.log(returnTen());
+console.log(returnTen());
+console.log(returnTen());
