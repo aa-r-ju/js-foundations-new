bf1f28c2dd42f47b714ebfde2e998af1a61d9da6
aa-r-ju <aarju3447@gmail.com>
2026-03-24 17:23:51 +0545

    feat: alternate

--- tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
+++ tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function alternate(fn) {
+  let count = 0;
+  return function () {
+    count++;
+    if (count % 2 !== 0) {
+      fn();
+    }
+  };
+}
