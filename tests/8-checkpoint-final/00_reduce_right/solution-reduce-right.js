28ea9c3511be0f7eeca928d7dfdb5bd532b39847
aa-r-ju <aarju3447@gmail.com>
2026-03-24 17:02:36 +0545

    feat: reduceRight

--- tests/8-checkpoint-final/00_reduce_right/solution-reduce-right.js
+++ tests/8-checkpoint-final/00_reduce_right/solution-reduce-right.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function reduceRight(arr, starting, fn) {
+  let acc = starting;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    acc = fn(acc, arr[i]);
+  }
+  return acc;
+}
+const stringConcat = (prev, curr) => {
+  return prev + curr;
+};
+
+console.log(reduceRight(["o", "l", "l", "e", "h"], "", stringConcat));
