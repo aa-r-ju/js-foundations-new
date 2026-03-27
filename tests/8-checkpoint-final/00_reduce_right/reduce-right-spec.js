0bf9b221afd015af745200883d94e2ad8c5a0ae4
aa-r-ju <aarju3447@gmail.com>
2026-03-27 20:54:14 +0545

    feat: VM(vending machine)

--- tests/8-checkpoint-final/00_reduce_right/reduce-right-spec.js
+++ tests/8-checkpoint-final/00_reduce_right/reduce-right-spec.js
@@ -1,31 +1,31 @@
 /* eslint-env jasmine */
 /* eslint-disable no-unused-vars, no-prototype-builtins, no-undef */
 
-describe('reduceRight takes an array, starting point, and combining function and applies the values in the array starting from the end', () => {
-  it('works with string concatenation', () => {
+describe("reduceRight takes an array, starting point, and combining function and applies the values in the array starting from the end", () => {
+  it("works with string concatenation", () => {
     const stringConcat = (prev, curr) => {
       return prev + curr;
     };
     const reducedResult = reduceRight(
-      ['o', 'l', 'l', 'e', 'h'],
-      '',
+      ["o", "l", "l", "e", "h"],
+      "",
       stringConcat
     );
-    expect(reducedResult).toBe('hello');
+    expect(reducedResult).toBe("hello");
   });
 
-  it('works with word frequency calculation', () => {
+  it("works with word frequency calculation", () => {
     const words = [
-      'I',
-      'wish',
-      'to',
-      'wish',
-      'the',
-      'wish',
-      'you',
-      'wish',
-      'to',
-      'wish',
+      "I",
+      "wish",
+      "to",
+      "wish",
+      "the",
+      "wish",
+      "you",
+      "wish",
+      "to",
+      "wish",
     ];
     const reducedResult = reduceRight(words, {}, (wordFrequencyObj, word) => {
       if (wordFrequencyObj[word] === undefined) {
@@ -38,7 +38,7 @@ describe('reduceRight takes an array, starting point, and combining function and
     expect(reducedResult).toEqual({ I: 1, wish: 5, to: 2, the: 1, you: 1 });
   });
 
-  it('does not modify the original array', () => {
+  it("does not modify the original array", () => {
     const arr = [4, 5, 6];
     const copyOfOriginal = arr.slice();
     const reducedResult = reduceRight(arr, 0, (total, num) => {
@@ -48,15 +48,15 @@ describe('reduceRight takes an array, starting point, and combining function and
     expect(arr).toEqual(copyOfOriginal);
   });
 
-  it('does not use Array.prototype.reduce or Array.prototype.reduceRight', () => {
-    spyOn(Array.prototype, 'reduce');
-    spyOn(Array.prototype, 'reduceRight');
+  it("does not use Array.prototype.reduce or Array.prototype.reduceRight", () => {
+    spyOn(Array.prototype, "reduce");
+    spyOn(Array.prototype, "reduceRight");
     const stringConcat = (prev, curr) => {
       return prev + curr;
     };
     const reducedResult = reduceRight(
-      ['o', 'l', 'l', 'e', 'h'],
-      '',
+      ["o", "l", "l", "e", "h"],
+      "",
       stringConcat
     );
 
--- tests/8-checkpoint-final/02_vending-machine/solution-vending-machine.js
+++ tests/8-checkpoint-final/02_vending-machine/solution-vending-machine.js
@@ -1,1 +1,32 @@
 /* eslint-disable no-unused-vars */
+class VM {
+  constructor(inventory) {
+    this.inventory = inventory;
+  }
+
+  sale(itemID) {
+    const item = this.inventory[itemID];
+
+    if (item && item.stock > 0) {
+      item.stock -= 1;
+      return `1 ${item.name} - Thank you and come again!`;
+    }
+  }
+
+  stockTotal() {
+    const total = Object.values(this.inventory).reduce(
+      (sum, item) => sum + item.stock,
+      0
+    );
+
+    return total === 0 ? "Out of Stock" : `${total} item(s)`;
+  }
+}
+
+let kk = new VM({
+  1: { name: "Sunny Delight", stock: 5 },
+  2: { name: "Diet Soda", stock: 3 },
+  3: { name: "Bottled Water", stock: 4 },
+});
+console.log(kk.sale(3));
+console.log(kk.stockTotal());
