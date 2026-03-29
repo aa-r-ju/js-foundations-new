15402622e99fcd4011c25577b29bbd32ed092396
aa-r-ju <aarju3447@gmail.com>
2026-03-29 15:47:16 +0545

    feat: aQuarter

--- tests/8-checkpoint-final/03_a-quarter/solution-a-quarter.js
+++ tests/8-checkpoint-final/03_a-quarter/solution-a-quarter.js
@@ -1,1 +1,24 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function aQuarter(fn) {
+  let count = 0;
+
+  return function (...args) {
+    count++;
+
+    if (count % 4 !== 0) {
+      return "something went wrong :/";
+    }
+
+    return fn(...args);
+  };
+}
+
+const plus = (x, y) => {
+  return x + y;
+};
+let kk = aQuarter(plus);
+
+console.log(kk(5, 5));
+console.log(kk(5, 5));
+console.log(kk(5, 5));
+console.log(kk(5, 5));
